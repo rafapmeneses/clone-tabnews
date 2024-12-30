9d8f2b81b097015dabb5367d96181f62cf25a2e7
rafapmeneses <rafaelp.m@hotmail.com>
2024-12-30 13:51:01 -0300

    Adicionado endpoint de migrations

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,10 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    connectionTimeoutMillis: 10000,
-    ssl: getSSLValues(),
-  });
+  let client;
 
   try {
-    await client.connect();
+    client = await getNewclient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
@@ -23,8 +15,24 @@ async function query(queryObject) {
   }
 }
 
+async function getNewclient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    connectionTimeoutMillis: 10000,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+}
+
 export default {
-  query: query,
+  query,
+  getNewclient,
 };
 
 function getSSLValues() {
@@ -33,5 +41,5 @@ function getSSLValues() {
       ca: process.env.POSTGRES_CA,
     };
   }
-  return process.env.NODE_ENV === "development" ? false : true;
+  return process.env.NODE_ENV === "production" ? true : false;
 }
