22a9ad8aace7e678496a53117cdff33834968f26
rafapmeneses <rafaelp.m@hotmail.com>
2025-11-10 22:02:37 -0300

    feat: implement api/v1/user endpoint with session renewal

--- models/session.js
+++ models/session.js
@@ -1,8 +1,41 @@
 import crypto from "node:crypto";
 import database from "infra/database.js";
+import { UnauthorizedError } from "infra/errors";
 
 //60 seconds, 60 minutos, 24 hours, 30 days, 1000 milliseconds
 const EXPIRATION_IN_MILLISECONDS = 60 * 60 * 24 * 30 * 1000; // 30 days in milliseconds
+
+async function findOneValidByToken(sessionToken) {
+  const sessionFound = await runSelectQuery(sessionToken);
+
+  return sessionFound;
+
+  async function runSelectQuery(sessionToken) {
+    const results = await database.query({
+      text: `
+        SELECT
+         *
+        FROM
+         sessions
+        WHERE
+         token = $1
+         AND expires_at > NOW()
+        LIMIT
+         1
+      ;`,
+      values: [sessionToken],
+    });
+
+    if (results.rowCount === 0) {
+      throw new UnauthorizedError({
+        message: "Usuário não possui sessão ativa.",
+        action: "Verifique se este usuário está logado e tente novamente.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
 
 async function create(userId) {
   const token = crypto.randomBytes(48).toString("hex");
@@ -29,8 +62,36 @@ async function create(userId) {
   }
 }
 
+async function renew(sessionId) {
+  const expires_at = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS);
+
+  const renewedSessionObject = runUpdateQuery(sessionId, expires_at);
+  return renewedSessionObject;
+
+  async function runUpdateQuery(sessionId, expires_at) {
+    const results = await database.query({
+      text: `
+        UPDATE
+          sessions
+        SET
+          expires_at = $2,
+          updated_at = NOW()
+        WHERE
+          id = $1
+        RETURNING
+          *
+      ;`,
+      values: [sessionId, expires_at],
+    });
+
+    return results.rows[0];
+  }
+}
+
 const session = {
   create,
+  renew,
+  findOneValidByToken,
   EXPIRATION_IN_MILLISECONDS,
 };
 
